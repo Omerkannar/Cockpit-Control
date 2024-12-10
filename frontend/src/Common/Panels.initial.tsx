0134578832b61d3a818b1b80ff931e329984c5c4
Omer Kannar <omerkannar@gmail.com>
2024-12-10 13:36:00 -0600

    Some minor modifications

--- frontend/src/Common/Panels.initial.tsx
+++ frontend/src/Common/Panels.initial.tsx
@@ -8,8 +8,18 @@ export const Cmds_ProgramInitialValues: Interface.Cmds_ProgramInterface['input']
      "KNOB_PRGM_IN":	"",
 }
 
+export const Epu_FuelInitialValues: Interface.Epu_FuelInterface['input'] = {
+     "EPU_FUEL_IN":	"",
+}
+
+export const Lg_ParkingInitialValues: Interface.Lg_ParkingInterface['input'] = {
+     "LG_PARKING_IN":	"",
+}
+
 export const initialValues: { [K in keyof Interface.InterfaceMap]: Interface.InterfaceMap[K]['input'] } = {
 	"LandingGear_Wheels":	LandingGear_WheelsInitialValues,
 	"Cmds_Program":	Cmds_ProgramInitialValues,
+	"Epu_Fuel":	Epu_FuelInitialValues,
+	"Lg_Parking":	Lg_ParkingInitialValues,
 }
 
